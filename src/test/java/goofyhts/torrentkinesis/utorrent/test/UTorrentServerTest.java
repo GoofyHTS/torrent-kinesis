a7698ac55b3c6d35505b4882d89d945b0325a22f
GoofyHTS <goofyhts@gmail.com>
2015-02-20 22:14:05 +0200

    renamed test resouces path

--- src/test/java/goofyhts/torrentkinesis/utorrent/test/UTorrentServerTest.java
+++ src/test/java/goofyhts/torrentkinesis/utorrent/test/UTorrentServerTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.*;
 
 public class UTorrentServerTest {
 	
-	private static final String basePath = "src\\test\\resources\\goofyhts\\torrent\\api\\utorrent\\test\\resources";
+	private static final String basePath = "src\\test\\resources\\goofyhts\\torrentkinesis\\utorrent\\test\\resources";
 	private static final String baseUrl = "http://unit.test:1234";
 	private static final String baseTokenPath = basePath + "/token.html";
 	private static final String baseTokenUrl = baseUrl + UTorrentServerConst.REQUEST_TOKEN_URL;
